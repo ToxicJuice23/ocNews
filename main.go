cacd3dd7db93568c1867699f155d2e960a1958ba
ToxicJuice23 <jujur2300@gmail.com>
2023-07-30 11:10:00 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"html/template"
+	"net/http"
+	"os"
+	"time"
+)
+
+func getAlerts() []string {
+	r := curl("https://www.octranspo.com/en/alerts", time.Second*20)
+	doc, err := goquery.NewDocumentFromReader(r.Body)
+	if err != nil {
+		fmt.Errorf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	var alerts []string
+	doc.Find(".alert").Find(".accordion").Each(func(i int, s *goquery.Selection) {
+		alerts = append(alerts, s.Text())
+	})
+	return alerts
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(os.Stdout, "Recieved request: %s\n", time.Now())
+	fd, err := os.ReadFile("template.html")
+	t, err := template.New("template").Parse(string(fd))
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Internal Server Error")
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return
+	}
+	t.Execute(w, getAlerts())
+}
+
+func main() {
+	fmt.Fprintf(os.Stdout, "Initializing server.\n")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", homeHandler)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = ":80"
+	} else {
+		port = ":" + port
+	}
+	fmt.Printf("Listening on port %s\n", port)
+	http.ListenAndServe(port, mux)
+}
